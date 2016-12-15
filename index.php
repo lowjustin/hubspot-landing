72e5c594220cf879f68c3d1a97b4fb3537fa689a
lowjustin <justin@pettycache.info>
2016-12-15 14:41:22 +0800

    First version

--- index.php
+++ index.php
@@ -7,8 +7,10 @@
 
     <title>Facets Singapore</title>
 
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+
     <!-- Bootstrap -->
-    <link href="css/style.css" rel="stylesheet">
+    <link href="assets/stylesheets/style.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -18,6 +20,20 @@
     <![endif]-->
   </head>
   <body class="container">
+
+    <section class="styleguide">
+      <h1 class="title title--xl">title--xl. Bootstrap heading <small>Secondary text</small></h1>
+      <h1 class="title title--l">title--l. Bootstrap heading <small>Secondary text</small></h1>
+      <h1 class="title title--m">title--l. Bootstrap heading <small>Secondary text</small></h1>
+
+      <p class="lead">Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>
+      <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla.</p>
+      <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Donec id elit non mi porta gravida at eget metus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+      
+      <p><small>This line of text is meant to be treated as fine print.</small></p>
+      <p><strong>rendered as bold text</strong></p>
+      <p><em>rendered as italicized text</em></p>
+    </section>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
