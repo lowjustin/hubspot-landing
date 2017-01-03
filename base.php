f11e23b36d33f7c72f70f166065e0c39346b2940
lowjustin <justin@pettycache.info>
2017-01-03 17:05:51 +0800

    Updated base component

---
+++ base.php
@@ -0,0 +1,11 @@
+<footer class="base">
+  <div class="section">
+    <h1>Back to Facets Singapore</h1>
+    <h2>Visit our website for more information</h2>
+    <a class="btn" href="#0">Visit Facets Singapore</a>
+  </div>
+
+  <div class="sub-section">
+    <p class="copyright">© 2016–2017 Facets Singapore</p>
+  </div>
+</footer>
