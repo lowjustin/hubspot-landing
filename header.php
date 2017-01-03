98f3f1b355566d74c0a21645c51f5337cb439f21
lowjustin <justin@pettycache.info>
2017-01-03 14:11:16 +0800

    Revised page-header into mast

--- header.php
+++ header.php
@@ -8,15 +8,7 @@
     <title>Facets Singapore</title>
 
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
-
-    <!-- Bootstrap -->
     <link href="assets/stylesheets/style.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    
   </head>
   <body class="container">
