33817301004188ab2bb546745dbb1a1cc4f86314
lowjustin <justin@pettycache.info>
2017-01-03 19:07:51 +0800

    Sections for Campaign Hero template

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <? include 'header.php' ?>
 <? include 'mast.php' ?>
 
+<main class="main">
+  <div class="section campaign-hero">
+    
+  </div>  
+  
+  <div class="section campaign-info">
+    
+  </div>
+</main>
+
 <? include 'base.php' ?>
 <? include 'footer.php' ?>
--- mast.php
+++ mast.php
@@ -1,10 +1,14 @@
 <div class="mast">
-  <a class="header__logo" href="#0">
-    <span class="img-rpl logo__mark">
-      Facets Singapore
-      <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="assets/images/logo.svg#facets"></use></svg>
-    </span>
-  </a>
-  <h1>Page title</h1>
-  <h2>Campaign preamble</h2>
+  <div class="section">
+    <a class="header__logo" href="#0">
+      <span class="img-rpl logo__mark">
+        Facets Singapore
+        <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="assets/images/logo.svg#facets"></use></svg>
+      </span>
+    </a>
+  </div>
+  <div class="section">
+    <h1>Page title</h1>
+    <h2>Campaign preamble</h2>
+  </div>
 </div>
