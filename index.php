a679fe1ab3e34f8ef41da75c7e6f7d8c1bef5292
lowjustin <justin@pettycache.info>
2017-01-03 12:54:47 +0800

    Split HTML sections into PHP includes

--- index.php
+++ index.php
@@ -1,42 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<? include 'header.php' ?>
+<? include 'mast.php' ?>
 
-    <title>Facets Singapore</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
-
-    <!-- Bootstrap -->
-    <link href="assets/stylesheets/style.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body class="container">
-
-    <section class="styleguide">
-      <h1 class="title title--xl">title--xl. Bootstrap heading <small>Secondary text</small></h1>
-      <h1 class="title title--l">title--l. Bootstrap heading <small>Secondary text</small></h1>
-
-      <p class="lead">Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>
-      <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla.</p>
-      <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Donec id elit non mi porta gravida at eget metus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
-      
-      <p><small>This line of text is meant to be treated as fine print.</small></p>
-      <p><strong>rendered as bold text</strong></p>
-      <p><em>rendered as italicized text</em></p>
-    </section>
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+<? include 'base.php' ?>
+<? include 'footer.php' ?>
---
+++ styleguide.php
@@ -0,0 +1,28 @@
+<? include 'header.php' ?>
+<? include 'mast.php' ?>
+
+  <section class="styleguide">
+    <h1 class="title">title--xl. Bootstrap heading <small>Secondary text</small></h1>
+    <h2 class="title">title--l. Bootstrap heading <small>Secondary text</small></h2>
+    <h3 class="title">title--l. Bootstrap heading <small>Secondary text</small></h3>
+
+    <p class="lead">Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>
+    <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla.</p>
+    <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Donec id elit non mi porta gravida at eget metus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+    
+    <p><small>This line of text is meant to be treated as fine print.</small></p>
+    <p><strong>rendered as bold text</strong></p>
+    <p><em>rendered as italicized text</em></p>
+  </section>
+
+  <section>
+    <div class="jumbotron">
+      <img src="http://placehold.it/680x200" alt="">
+      <h1 class="title title--xl">Campaign hero</h1>
+      <p>Campaign description</p>
+      <p><a class="btn btn-primary btn-lg" href="#" role="button">HubSpot button</a></p>
+    </div>
+  </section>
+
+<? include 'base.php' ?>
+<? include 'footer.php' ?>
