54b982f5a33a14dbd82c7070e4f1f1b0431182d7
lowjustin <justin@pettycache.info>
2017-01-04 16:10:52 +0800

    Campaign info

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <main class="main">
   <div class="section campaign-hero">
     <div class="sub-section campaign-hero__banner">
-      <img src="https://placeimg.com/1140/300/any alt=""">
+      <img src="https://facetsingapore.com/thumbs/content__highlight-wholesale-a4cd4f3602574a75aa66d5cbe8275b52.jpg" alt="">
     </div>
     <div class="sub-section campaign-hero__description">
       <h1>Hero title</h1>
@@ -13,8 +13,26 @@
     </div>
   </div>  
 
-  <div class="section campaign-info">
-    
+  <div class="container-fluid">
+    <div class="section campaign-info row">
+      <div class="sub-section campaign-info__description col-md-8">
+        <h1>Description about the campaign</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at dictum ante. Morbi nulla nunc, luctus nec urna at, tincidunt tristique ligula. Vestibulum efficitur purus ut varius accumsan. Fusce nec nisi eu nisi fringilla vestibulum eget eget dolor. Etiam scelerisque orci neque, at ultricies ex commodo ut.</p>
+        <p>Donec mollis, libero nec pharetra aliquet, metus metus auctor arcu, eu dictum velit ipsum id arcu. Pellentesque dignissim est in metus efficitur pretium. Suspendisse potenti. Sed convallis risus felis, nec consectetur nunc imperdiet non.</p>
+        <p>Praesent consequat neque turpis, a vestibulum massa bibendum nec. In iaculis augue sem, eget fermentum dui tincidunt ac. In finibus turpis nec lectus tincidunt semper. Aliquam ut tempus massa. Aliquam convallis turpis et tellus dapibus interdum.</p>
+      </div>
+      <div class="sub-section campaign-info__terms col-md-4">
+        <h1>Terms and conditions</h1>
+        <ul>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+          <li>Vestibulum suscipit lorem quis hendrerit rhoncus.</li>
+          <li>Duis ornare quam ac velit dignissim congue.</li>
+          <li>Curabitur at ligula posuere, placerat erat vitae, lobortis orci.</li>
+          <li>Nulla sagittis sem et sem laoreet congue.</li>
+          <li>Quisque porta ligula sit amet aliquet finibus.</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </main>
 
