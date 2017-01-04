3f3a3cb8f4797388434187c5a337444eeefb9e28
lowjustin <justin@pettycache.info>
2017-01-04 15:13:24 +0800

    Campaign hero

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 
 <main class="main">
   <div class="section campaign-hero">
-    
+    <div class="sub-section campaign-hero__banner">
+      <img src="https://placeimg.com/1140/300/any alt=""">
+    </div>
+    <div class="sub-section campaign-hero__description">
+      <h1>Hero title</h1>
+      <h2>Caption</h2>
+      <a class="btn btn-primary" href="#0">HubSpot button</a>
+    </div>
   </div>  
-  
+
   <div class="section campaign-info">
     
   </div>
